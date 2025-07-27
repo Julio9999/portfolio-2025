6b45e031c8bfdf58c742481e95dd5df8da96d818
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2025-07-27 11:34:51 -0300

    pseudo clase active para dispositivos con pantalla tactil

--- src/modules/about-me/pages/about-me.tsx
+++ src/modules/about-me/pages/about-me.tsx
@@ -2,8 +2,8 @@ export const AboutMePage = () => {
     return (
         <section className="flex items-center justify-center">
             <div className="max-w-md w-full bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg p-8 text-center space-y-6">
-                <h1 className="text-4xl font-bold mb-4">Sobre mí</h1>
-                <p className="max-w-xl text-lg">
+                <h1 className="text-4xl md:text-5xl font-bold mb-4">Sobre mí</h1>
+                <p className="text-sm md:text-base text-white/80">
                     ¡Hola! Soy un desarrollador apasionado por crear aplicaciones web dinámicas y responsivas.
                     Disfruto trabajar con tecnologías modernas y aprender constantemente nuevas habilidades para mejorar mi trabajo.
                 </p>
--- src/modules/home/components/icons-container/icons-container.tsx
+++ src/modules/home/components/icons-container/icons-container.tsx
@@ -1,54 +1,56 @@
-import { RiNextjsFill, RiNodejsFill, RiReactjsFill, RiTailwindCssFill } from "react-icons/ri"
-import { SiNestjs, SiTypescript } from "react-icons/si"
-
-import { IconContainer } from "@/components/icon-container"
+import {
+    RiNextjsFill,
+    RiNodejsFill,
+    RiReactjsFill,
+    RiTailwindCssFill
+} from "react-icons/ri";
+import { SiNestjs, SiTypescript } from "react-icons/si";
+import { IconContainer } from "@/components/icon-container";
 
 export const IconsContainer = () => {
-
     return (
         <div className="flex flex-wrap justify-center md:justify-start items-center gap-6 p-4">
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#3178c6]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#3178c6] active:shadow-[0_0_30px_#3178c6]">
                 <SiTypescript
                     size={64}
-                    className="text-white group-hover:text-[#3178c6] transition-colors duration-300"
+                    className="text-white transition-colors duration-300 group-hover:text-[#3178c6] group-active:text-[#3178c6]"
                 />
             </IconContainer>
 
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#22d3ee]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#22d3ee] active:shadow-[0_0_30px_#22d3ee]">
                 <RiReactjsFill
                     size={64}
-                    className="text-white transform transition-all duration-1000 group-hover:text-[#22d3ee] group-hover:rotate-[360deg]"
+                    className="text-white transform transition-all duration-1000 group-hover:text-[#22d3ee] group-hover:rotate-[360deg] group-active:text-[#22d3ee] group-active:rotate-[360deg]"
                 />
             </IconContainer>
 
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#38bdf8]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#38bdf8] active:shadow-[0_0_30px_#38bdf8]">
                 <RiTailwindCssFill
                     size={64}
-                    className="text-white group-hover:text-[#38bdf8] transition-colors duration-300"
+                    className="text-white transition-colors duration-300 group-hover:text-[#38bdf8] group-active:text-[#38bdf8]"
                 />
             </IconContainer>
 
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#d4d4d4]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#d4d4d4] active:shadow-[0_0_30px_#d4d4d4]">
                 <RiNextjsFill
                     size={64}
-                    className="text-white group-hover:text-[#d4d4d4] transition-colors duration-300"
+                    className="text-white transition-colors duration-300 group-hover:text-[#d4d4d4] group-active:text-[#d4d4d4]"
                 />
             </IconContainer>
 
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#10b981]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#10b981] active:shadow-[0_0_30px_#10b981]">
                 <RiNodejsFill
                     size={64}
-                    className="text-white group-hover:text-[#10b981] transition-colors duration-300"
+                    className="text-white transition-colors duration-300 group-hover:text-[#10b981] group-active:text-[#10b981]"
                 />
             </IconContainer>
 
-            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#dc2626]">
+            <IconContainer className="group transition-shadow duration-300 hover:shadow-[0_0_30px_#dc2626] active:shadow-[0_0_30px_#dc2626]">
                 <SiNestjs
                     size={64}
-                    className="text-white group-hover:text-[#dc2626] transition-colors duration-300"
+                    className="text-white transition-colors duration-300 group-hover:text-[#dc2626] group-active:text-[#dc2626]"
                 />
             </IconContainer>
         </div>
-    )
-
-}
+    );
+};
