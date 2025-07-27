c0aeaa4b7cad93c8a3884095c3a5aabba924a968
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2025-07-27 11:18:03 -0300

    contact page added

---
+++ src/app/(common-layout)/contact/page.tsx
@@ -0,0 +1,9 @@
+import { ContactPage } from "@/modules/contact/pages/contact-page";
+
+const Page = () => {
+    return (
+        <ContactPage />
+    )
+}
+
+export default Page;    
--- src/modules/about-me/pages/about-me.tsx
+++ src/modules/about-me/pages/about-me.tsx
@@ -1,11 +1,13 @@
 export const AboutMePage = () => {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mb-4">About Me</h1>
-            <p>
-                Hello! I&apos;m a passionate developer with a love for creating dynamic and responsive web applications. 
-                I enjoy working with modern technologies and continuously learning new skills to enhance my craft.
-            </p>
-        </div>
+        <section className="flex items-center justify-center">
+            <div className="max-w-md w-full bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-lg p-8 text-center space-y-6">
+                <h1 className="text-4xl font-bold mb-4">Sobre mí</h1>
+                <p className="max-w-xl text-lg">
+                    ¡Hola! Soy un desarrollador apasionado por crear aplicaciones web dinámicas y responsivas.
+                    Disfruto trabajar con tecnologías modernas y aprender constantemente nuevas habilidades para mejorar mi trabajo.
+                </p>
+            </div>
+        </section>
     );
 }
